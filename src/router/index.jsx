607a76acaf1ae88cd56393e719d972b1dbcd9f7d
Luiz Antônio <la2004.o.sc@gmail.com>
2024-12-04 20:17:22 -0300

    feat: :sparkles: estrutura de paginas

---
+++ src/router/index.jsx
@@ -0,0 +1,14 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import { Page } from "../pages";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:  <Page></Page>,
+    },
+    
+]);
+
+export default router;
